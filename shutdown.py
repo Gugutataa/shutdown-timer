b14f0ac254c04753cdf859d10717a16d7fb8a524
Gugutataa <111111608+Gugutataa@users.noreply.github.com>
2022-08-19 19:23:27 +0200

    first commit

---
+++ shutdown.py
@@ -0,0 +1,10 @@
+import time
+import pyautogui
+timer = input("Enter time until the shutdown happens:")
+import win32gui, win32con
+the_program_to_hide = win32gui.GetForegroundWindow()
+win32gui.ShowWindow(the_program_to_hide , win32con.SW_HIDE)
+time.sleep(timer)
+pyautogui.hotkey("win", "m")
+pyautogui.hotkey("alt", "f4")
+pyautogui.press("enter")
